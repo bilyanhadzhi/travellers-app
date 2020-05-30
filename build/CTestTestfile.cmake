c23290f20d243de70c7118aad723aef2f5dee51a
bilyanhadzhi <bilyanhadzhi@gmail.com>
2020-05-30 18:14:40 +0300

    Add CMake to .gitignore

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/bilyan/Coding/Projects/FMI/travellers_app
-# Build directory: /home/bilyan/Coding/Projects/FMI/travellers_app/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
